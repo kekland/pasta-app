6703272ea6ab0f12091b89ffd2a49ba788f7198c
kekland <kk.erzhan@gmail.com>
2018-06-03 17:25:55 +0600

    Initial commit

---
+++ android/app/src/main/java/com/example/pastaapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pastaapp;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'registration_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Pasta',
+      theme: new ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: new RegistrationPage(),
+    );
+  }
+}

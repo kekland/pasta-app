5ba2941f3a1eb13328e3451031a2ce43518bf978
kekland <kk.erzhan@gmail.com>
2018-06-03 18:06:28 +0600

    Redoing onboarding

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -1,78 +1,29 @@
 import 'package:flutter/material.dart';
-import 'page_transformer.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        constraints: new BoxConstraints(minHeight: double.INFINITY, minWidth: double.INFINITY),
-        color: Colors.white,
-        child: PageTransformer(
-          pageViewBuilder: (context, pageVisibilityResolver) {
-            return new PageView.builder(
-              itemBuilder: (context, index) {
-                final pageVisibility = pageVisibilityResolver.resolvePageVisibility(index);
-
-                // Use these two properties to transform your "Hello World" text widget!
-                // In this example, the text widget fades in and out of view, since we use
-                // the visibleFraction property, which can be between 0.0 - 1.0.
-                final position = pageVisibility.pagePosition;
-                final visibleFraction = pageVisibility.visibleFraction;
-
-                return new Opacity(
-                  opacity: visibleFraction,
-                  child: new Text('Hello World!'),
-                );
-              },
-            );
-          },
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return new OnboardingPageState();
   }
 }
 
-class WelcomingPage extends StatelessWidget {
-  final Function onFABPressed;
-  WelcomingPage({onFABPressed});
-
+class OnboardingPageState extends State<OnboardingPage> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: new EdgeInsets.all(64.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Pasta',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.teal,
-                  fontSize: 48.0,
-                ),
-              ),
-              Text(
-                'Welcome to Pasta!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.teal.shade400,
-                  fontSize: 20.0,
-                ),
-              ),
-            ],
-          ),
-          FloatingActionButton(
-            onPressed: onFABPressed,
-            child: Icon(Icons.chevron_right),
-          ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          new Container(
+            color: Colors.blue,
+            child: Column(
+              children: [
+                Icon(
+                  Icons.access_alarm,
+                  size: 200.0,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );

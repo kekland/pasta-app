91eb4667302bc9fdcdb03a33a837b76ab0382787
kekland <kk.erzhan@gmail.com>
2018-06-03 17:53:40 +0600

    Added PageTransformer component

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'page_transformer.dart';
 
 class OnboardingPage extends StatelessWidget {
   @override
@@ -7,7 +8,26 @@ class OnboardingPage extends StatelessWidget {
       body: Container(
         constraints: new BoxConstraints(minHeight: double.INFINITY, minWidth: double.INFINITY),
         color: Colors.white,
-        child: WelcomingPage(),
+        child: PageTransformer(
+          pageViewBuilder: (context, pageVisibilityResolver) {
+            return new PageView.builder(
+              itemBuilder: (context, index) {
+                final pageVisibility = pageVisibilityResolver.resolvePageVisibility(index);
+
+                // Use these two properties to transform your "Hello World" text widget!
+                // In this example, the text widget fades in and out of view, since we use
+                // the visibleFraction property, which can be between 0.0 - 1.0.
+                final position = pageVisibility.pagePosition;
+                final visibleFraction = pageVisibility.visibleFraction;
+
+                return new Opacity(
+                  opacity: visibleFraction,
+                  child: new Text('Hello World!'),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
